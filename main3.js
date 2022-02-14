8f07c5ad30c1ee4e49213d4c7ad5b91e7f05a2f0
matias <casla_matias@hotmail.com>
2022-02-14 19:48:43 -0300

    Desafio Modulo 1

--- main3.js
+++ main3.js
@@ -1,3 +1,4 @@
+//Variables
 let entradasEventoA = "La renga"
 let precioEntradasEventoA = 1000
 let stockEntradasEventoA = 10
@@ -7,39 +8,45 @@ let precioEntradasEventoB = 3000
 let stockEntradasEventoB = 10
 let cantidadComprada  //?
 let precioTotal = 0
+//Fin variables
 
+//Funciones
+function sinStock (artista){
+    alert("Solo puede comprar hasta 10 entradas de " + artista)
+} 
 
-let nombreUsuario = prompt("Hola, bienvenido! \n Ingrese su nombre por favor:")
+function conStock(precio, artista, stock){
+
+    precioTotal += cantidadComprada * precio
+    alert("Se agrego al carrito " + cantidadComprada + " entradas para ver a " + artista + " en el Cosquin Rock 2022")
+    stock -= cantidadComprada
+    console.log(nombreUsuario + " compró " + cantidadComprada + " entradas de " + artista + " quedando un stock final de: " + stock);
+}
+
+function compra(precio, artista, stock){
+    
+    cantidadComprada = parseInt(prompt("Ingrese la cantidad de entradas que quiere"))
+    if (cantidadComprada <= 10){
+        conStock(precio, artista, stock)
+    }
+    else{
+        sinStock(artista)
+    }
+}
+//Fin funciones
+
+let nombreUsuario = prompt("Hola, bienvenido! \nIngrese su nombre por favor:")
+
 let cantidadDeLaCompra = parseInt(prompt("Hola " + nombreUsuario + "! indique de cuantos artistas diferentes quiere las entradas, en este sitio vendemos entradas para Divididos y La renga"))
 if (cantidadDeLaCompra <= 2){
 
 for(let i = 0; i < cantidadDeLaCompra; i++){
-
     let nombreCompra = prompt("Ingrese el nombre del artista que quiere comprar")
     if (nombreCompra === entradasEventoA){
-        cantidadComprada = parseInt(prompt("Ingrese la cantidad de entradas que quiere"))
-        if (cantidadComprada <= stockEntradasEventoA){
-            precioTotal += cantidadComprada * precioEntradasEventoA
-            alert("Se agrego al carrito " + cantidadComprada + " entradas para ver a " + entradasEventoA + " en el Cosquin Rock 2022")
-            stockEntradasEventoA = stockEntradasEventoA - cantidadComprada
-            console.log(nombreUsuario + " compró " + cantidadComprada + " entradas de " + entradasEventoA + " quedando un stock final de: " + stockEntradasEventoA);
-        }
-        else{
-            alert("Solo puede comprar hasta 10 entradas")
-        }
+        compra (precioEntradasEventoA, entradasEventoA, stockEntradasEventoA)
     }
     else if (nombreCompra === entradasEventoB){
-        
-        cantidadComprada = parseInt(prompt("Ingrese la cantidad de entradas que quiere"))
-        if (cantidadComprada <= stockEntradasEventoB){
-            precioTotal += cantidadComprada * precioEntradasEventoB
-            alert("Se agrego al carrito " + cantidadComprada + " entradas para ver a " + entradasEventoB + " en el Cosquin Rock 2022")
-            stockEntradasEventoB = stockEntradasEventoB - cantidadComprada
-            console.log(nombreUsuario + " compró " + cantidadComprada + " entradas de " + entradasEventoB + " quedando un stock final de: " + stockEntradasEventoB)
-        }
-        else{
-            alert("Solo puede comprar hasta 10 entradas")
-        }
+        compra (precioEntradasEventoB, entradasEventoB, stockEntradasEventoB)
     }
     else{
         alert("No tenemos entradas para ese artista")
