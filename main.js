23890cb57cfb6edc03f1f84fa708b6e2c4e8d98e
matias <casla_matias@hotmail.com>
2022-03-17 22:25:34 -0300

    correciones desafio eventos

--- main.js
+++ main.js
@@ -8,20 +8,14 @@ class Entradas {
     }
 }
 
-class Usuarios {
-    constructor (nombreUsuario){
-        this.nombre = nombreUsuario;
-    }
-
-}
 
 const eventoA = new Entradas ("La renga", 1000, 10, "La renga 20/02 - Galpón de Hurlingham", "../public/img/entradasEventoUno.jpeg");
 const eventoB = new Entradas ("Divididos", 3000, 10, "Divididos 14/05 - Teatro de Flores", "../public/img/entradasEventoDos.png");
-const usuarios = new Usuarios ();
 
 const listaEntradas = [eventoA, eventoB];
-const listaUsuarios = [usuarios];
 
+
+//DOM
 for(const place of listaEntradas){
     let contenedor = document.getElementById("places");
     let lugar = document.createElement("option");
@@ -44,8 +38,8 @@ for(const precio of listaEntradas){
 for (const entrada of listaEntradas){
     let contenedor = document.getElementById ("cards-container");
     
-    let prueba = document.createElement("div");
-     prueba.innerHTML = `<div class="eventoUno" id="info-evento1">
+    let card = document.createElement("div");
+     card.innerHTML = `<div class="eventoUno" id="info-evento1">
                             <img src=${entrada.img} class="img-fluid" alt="...">
                             <hr class="hr">
                             <p>${entrada.nombre}<br>
@@ -55,109 +49,31 @@ for (const entrada of listaEntradas){
                             <hr class="hr">
                         </div>`;  
 
-    contenedor.append(prueba);
+    contenedor.append(card);
 }
 
+//Eventos
+function respuestaClick () {
 
-let inputEmail = document.getElementById("exampleFormControlInput1");
-inputEmail.addEventListener ("change", envioMail);
-function envioMail () {
-    console.log(`La entrada fue enviada a ${inputEmail.value}`);
+    let optionEvento = document.getElementById("places").value;
+    let inputCantidadEntradas = document.getElementById("cantidad-entradas").value;
+    let inputEmail = document.getElementById("exampleFormControlInput1").value
+    let eventoQueQuiereUsuario = listaEntradas.find(evento => evento.lugar === optionEvento)
+    let total = inputCantidadEntradas * eventoQueQuiereUsuario.precio   
+
+    Swal.fire({
+    title: 'Gracias por su compra', 
+    text: `El total es: $${total}` ,
+    icon: 'success',
+    footer: `Se envió a: ${inputEmail} el QR para ingresar al show`,
+    confirmButtonText: 'Confirmar'
+    })
+
 }
 
-let inputCantidadEntradas = document.getElementById("cantidad-entradas");
-inputCantidadEntradas.addEventListener ("change", cantidadEntradas);
-function cantidadEntradas () {
-    console.log(`compraste ${inputCantidadEntradas.value}`);
-}
-
-let optionEvento = document.getElementById("places");
-optionEvento.addEventListener ("change", lugar);
-
-function lugar (){
-    console.log(optionEvento.value);
-}
-
-let optionValor = document.getElementById("precio");
-optionValor.addEventListener ("change", valor);
-
-function valor (){
-    console.log(optionValor.value);
-}
 
 let boton = document.getElementById("btn-enviar");
 boton.addEventListener("click", respuestaClick);
 
-function respuestaClick () {
-    console.log(`Gracias por su compra! Adiquiriste ${inputCantidadEntradas.value} entradas para ${optionEvento.value} y seran enviadas a ${inputEmail.value}`);
-}
 
 
-let cantidadComprada = 0;
-let precioTotal = 0;
-
-let contador = 0;
-let listaArtistas = "Hola bienvenido! acá vendemos entradas para estos artistas: ";
-
-for(const artista of listaEntradas){
-    contador++;
-    listaArtistas += "\n" + contador + " " + artista.nombre;
-}
-
-function listaEventos (){
-    alert(listaArtistas);
-}
-
-function sinStock (artista){
-    alert("Solo puede comprar hasta 10 entradas de " + artista);
-} 
-
-function conStock(precio, artista, stock){
-
-    precioTotal += cantidadComprada * precio;
-    alert("Se agrego al carrito " + cantidadComprada + " entradas para ver a " + artista + " en el Cosquin Rock 2022");
-    stock -= cantidadComprada;
-    console.log("compraron " + cantidadComprada + " entradas de " + artista + " quedando un stock final de: " + stock);
-}
-
-function compra(precio, artista, stock){
-    
-    cantidadComprada = parseInt(prompt("Ingrese la cantidad de entradas que quiere"));
-    if (cantidadComprada <= 10){
-        conStock(precio, artista, stock);
-    }
-    else{
-        sinStock(artista);
-        compra(precio, artista, stock);
-    }
-}
-
-/* listaUsuarios.push(new Usuarios(prompt("Hola, bienvenido! \nIngrese su nombre por favor:")));
-listaEventos();
-let cantidadDeLaCompra = parseInt(prompt("Ahora pongamos:\n1: Si queres para un artista\n2: Si queres para ambos."));
-
-if (cantidadDeLaCompra <= 2){
-
-    for(let i = 0; i < cantidadDeLaCompra; i++){
-        let nombreCompra = prompt("Ingrese el nombre del artista que quiere comprar");
-
-        let productoBuscado = listaEntradas.find(artista => artista.nombre == nombreCompra);
-        console.log(productoBuscado);
-        
-
-        if ((nombreCompra === listaEntradas[0].nombre) || (nombreCompra === "la renga")){
-            compra (listaEntradas[0].precio, listaEntradas[0].nombre, listaEntradas[0].stock);
-        }
-        else if ((nombreCompra === eventoB.nombre) || (nombreCompra === "divididos")){
-            compra (listaEntradas[1].precio, listaEntradas[1].nombre, listaEntradas[1].stock);
-        }
-        else{
-            alert("No tenemos entradas para ese artista"); 
-        }
-    }
-}
-else{
-    alert("Solo tenemos para dos artistas");
-    
-}
-alert("Muchas gracias por tu compra! Su saldo a pagar es: \n" + "$" + precioTotal); */
